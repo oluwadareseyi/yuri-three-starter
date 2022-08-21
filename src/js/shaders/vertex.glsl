479ad4538483374132aff6d7765eafc9af12e6ed
Oluwadare Seyi <oluwadareseyii@gmail.com>
2022-08-21 19:03:01 +0100

    feat: update vertex shader

--- src/js/shaders/vertex.glsl
+++ src/js/shaders/vertex.glsl
@@ -1,10 +1,10 @@
 varying vec3 vNormal;
 varying vec2 vUv;
 
-void main () {
-  vec4 projectedPosition = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-
-  gl_Position = projectedPosition;
-
-  vUv = uv;
+void main(){
+  vec4 mvPosition=modelViewMatrix*vec4(position,1.);
+  
+  gl_Position=projectionMatrix*mvPosition;
+  
+  vUv=uv;
 }
